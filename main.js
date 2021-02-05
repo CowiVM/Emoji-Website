c35f79384d7c1594d80a8707cbdf73136f19dbc4
CowiVM <72988033+CowiVM@users.noreply.github.com>
2021-02-05 12:13:30 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,26 +1,25 @@
-prediction_1 = ""
-prediction_2 = ""
+prediction = ""
 
 Webcam.set({
-    width:350,
+    width: 350,
     height: 300,
     image_format: "png",
     png_quality: 90
 })
 
-camera = document.getElementById("camera");
+camera = document.getElementById("camera")
 
 Webcam.attach(camera);
 
 function capture_image() {
-    Webcam.snap(function(data_uri) {
+    Webcam.snap(function(data_uri){
         document.getElementById("result").innerHTML = "<img id = 'captured_image' src = '" + data_uri + "'/>";
     })
 }
 
-console.log("ml5 version", ml5.version);
+console.log("ml5 version:", ml5.version);
 
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/LsoKpFs8J/model.json',modelLoaded)
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/LZTk6KGME/model.json',modelLoaded)
 
 function modelLoaded() {
     console.log("Model has successfully loaded");
@@ -28,9 +27,8 @@ function modelLoaded() {
 
 function speak() {
     synth = window.speechSynthesis;
-    speak_data_1 = "The first prediction is " + prediction_1;
-    speak_data_2 = "And the second prediction is " + prediction_2;
-    utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
+    speak_data_1 = "The prediction is " + prediction;
+    utterThis = new SpeechSynthesisUtterance(speak_data_1);
     synth.speak(utterThis)
 }
 
@@ -44,41 +42,27 @@ function getResult(error, results) {
         console.error(error);    
     } else {
         console.log(results);
-        document.getElementById("result_emotion_name").innerHTML = results[0].label;
-        document.getElementById("result_emotion_name2").innerHTML = results[1].label;
-        prediction_1 = results[0].label;
-        prediction_2 = results[1].label;
+        document.getElementById("result_hand_gesture").innerHTML = results[0].label;
+        prediction = results[0].label;
         speak();
-        if (results[0].label == "Happy") {
-            document.getElementById("result_emoji").innerHTML = "&#128512;"
+        if (results[0].label == "Victory") {
+            document.getElementById("result_hand_gesture").innerHTML = "&#9996;"
         }
         
-        if (results[0].label == "Sad") {
-            document.getElementById("result_emoji").innerHTML = "&#128546;"
+        if (results[0].label == "Not Good") {
+            document.getElementById("result_hand_gesture").innerHTML = "&#128078;"
         }
 
-        if (results[0].label == "Angry") {
-            document.getElementById("result_emoji").innerHTML = "&#128545;"
+        if (results[0].label == "Yo") {
+            document.getElementById("result_hand_gesture").innerHTML = "&#129304;"
         }
 
-        if (results[0].label == "Crying") {
-            document.getElementById("result_emoji").innerHTML = "&#128557;"
+        if (results[0].label == "Nice") {
+            document.getElementById("result_hand_gesture").innerHTML = "&#128076;"
         }
 
-        if (results[1].label == "Happy") {
-            document.getElementById("result_emoji2").innerHTML = "&#128512;"
-        }
-        
-        if (results[1].label == "Sad") {
-            document.getElementById("result_emoji2").innerHTML = "&#128546;"
-        }
-
-        if (results[1].label == "Angry") {
-            document.getElementById("result_emoji2").innerHTML = "&#128545;"
-        }
-
-        if (results[1].label == "Crying") {
-            document.getElementById("result_emoji2").innerHTML = "&#128557;"
+        if (results[0].label == "Amazing") {
+            document.getElementById("result_hand_gesture").innerHTML = "&#128077;"
         }
     }
     
